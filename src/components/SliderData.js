501f4beb7bae0038b55e9f2dd3fea9a563c27eb6
Dayany Anaile <daya.anaile@gmail.com>
2021-04-25 16:42:59 -0400

    Image Carousel

---
+++ src/components/SliderData.js
@@ -0,0 +1,11 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDh8NnNNVmpUTFNrZVF8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },    
+    {
+        image: 'https://images.unsplash.com/photo-1596016691838-70260f1e21c6?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8NnNNVmpUTFNrZVF8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'    
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1568824626196-bb4b0ee11646?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60'
+    }
+]
